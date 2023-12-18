434462ff7e547643423aee3dbd4a6ee98db0dd4e
Electric Worry <electricworry@users.noreply.github.com>
2023-12-18 22:26:20 +0000

    Fix mender patches for sunxi.

--- meta-mender-sunxi/recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ meta-mender-sunxi/recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -1,7 +1,8 @@
 run mender_setup;
-setenv bootargs console=${console} console=tty1 root=${mender_kernel_root} rootwait panic=10 ${extra}
-mmc dev ${mender_uboot_dev};
-load ${mender_uboot_root} ${fdt_addr_r} /boot/${fdtfile};
-load ${mender_uboot_root} ${kernel_addr_r} /boot/Image;
-booti ${kernel_addr_r} - ${fdt_addr_r};
-run mender_try_to_recover;
+setenv bootargs root=${mender_kernel_root} ${bootargs};
+if test "${fdt_addr_r}" != ""; then
+    load ${mender_uboot_root} ${fdt_addr_r} /boot/${mender_dtb_name};
+fi;
+load ${mender_uboot_root} ${kernel_addr_r} /boot/${mender_kernel_name};
+${mender_boot_kernel_type} ${kernel_addr_r} - ${fdt_addr_r};
+run mender_try_to_recover
--- meta-mender-sunxi/recipes-bsp/u-boot/u-boot_%.bbappend
+++ meta-mender-sunxi/recipes-bsp/u-boot/u-boot_%.bbappend
@@ -4,9 +4,9 @@ MENDER_UBOOT_AUTO_CONFIGURE = "0"
 
 BOOTENV_SIZE ?= "0x20000"
 
-SRC_URI += "\
+SRC_URI:append = "\
     file://0001-configs-sunxi-add-Mender-required-options.patch \
     file://0002-env-Kconfig-remove-defaults-for-SUNXI.patch \
-    file://0003-sunxi-common-Increase-SIZE_LIMIT-to-fit-with-mender-.patch \
-    file://0004-sunxi-common-Use-for-arm64-same-path-as-for-arm.patch \
+    file://0003-sunxi-common-Use-for-arm64-same-path-as-for-arm.patch \
+    file://0004-configs-orangepi-zero3-defconfig-env-size.patch \
 "
